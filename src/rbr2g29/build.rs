214c125a03f2995f103960e8e788d0bbd43f5a73
MajGucek <113456426+MajGucek@users.noreply.github.com>
2024-06-03 13:18:40 +0200

    Update build.rs

--- src/rbr2g29/build.rs
+++ src/rbr2g29/build.rs
@@ -1,11 +1,3 @@
-use winres::WindowsResource;
-
 fn main() {
-    WindowsResource::new()
-        .set("FileDescription", "RBR G29 LED RPM")
-        .set("ProductVersion", "1.0.0")
-        .set("OriginalFilename", "RBR2G29")
-        .set("ProductName", "RBR2G29")        
-        .compile()
-        .expect("winres");
+
 }
