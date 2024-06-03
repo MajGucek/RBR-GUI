dfaaff7a130eb96a921c434085200522a690ac35
MajGucek <113456426+MajGucek@users.noreply.github.com>
2024-06-03 13:46:12 +0200

    Update leds.rs

--- src/common/leds.rs
+++ src/common/leds.rs
@@ -1,6 +1,7 @@
 use crate::common::rpm::RPM;
 use core::u8::{MAX, MIN};
 use hidapi::HidDevice;
+use crate::common::telemetry::Telemetry;
 
 use super::{rbr::RBR, util::RBR2G29Result};
 
@@ -16,7 +17,8 @@ impl LEDS {
 
 
     pub fn update(&mut self, data: &[u8]) {
-        self.rpm.update(data);
+        //self.rpm.update(data);
+        let telemetry: Telemetry = deserialize(&data).unwrap();
     }
 
 
