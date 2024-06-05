da0b006e4aea4f39bf976413cddf8d37bf170524
MajGucek <maj.gucek@gmail.com>
2024-06-05 20:38:28 +0200

    creating ui

--- src/main.rs
+++ src/main.rs
@@ -7,14 +7,15 @@ use telemetry::Telemetry;
 
 // UI
 use bevy::{prelude::*};
-use egui::{pos2, Color32, Pos2, Rect, Rounding, Vec2};
 use bevy_egui::{EguiContexts, EguiPlugin};
+use egui::{Color32, Frame, Mesh, Pos2, Rect, Rounding, TextureId, Vec2, Widget};
 //use bevy::time::common_conditions::on_timer;
 //use bevy::utils::Duration;
 
 const PORT: &str = "127.0.0.1:6779";
 const WIDTH: f32 = 400.0;
-const HEIGHT: f32 = 100.0;
+const HEIGHT: f32 = 400.0;
+const ZERO: Pos2 = Pos2::new(0.0, 0.0);
 
 
 #[derive(Resource)]
@@ -68,7 +69,12 @@ impl Default for Socket {
     }
 }
 
-
+#[derive(States, Debug, Clone, PartialEq, Eq, Hash)]
+enum DisplayState {
+    Main,
+    Tyres,
+    Pedals,
+}
 
 
 fn main() {
@@ -76,6 +82,7 @@ fn main() {
         .insert_resource(ClearColor(Color::NONE))
         .add_plugins(DefaultPlugins)
         .add_plugins(EguiPlugin)
+        .insert_state(DisplayState::Main)
         .init_resource::<Socket>()
         .init_resource::<Data>()
         .init_resource::<RBR>()
@@ -89,9 +96,99 @@ fn main() {
                     )
         )
         */
+        .add_systems(Update, 
+            (
+                main_menu.run_if(in_state(DisplayState::Main)),
+                pedal_menu.run_if(in_state(DisplayState::Pedals)),
+                tyre_menu.run_if(in_state(DisplayState::Tyres))
+        ))
         .add_systems(Startup, init)
-        .add_systems(Update, ui_handler)
         .run();
+}
+
+fn tyre_menu(
+    mut windows: Query<&mut Window>,
+    mut egui_ctx: EguiContexts,
+    mut next_state: ResMut<NextState<DisplayState>>,
+) {
+    let mut window = windows.single_mut();
+    window.resolution.set(WIDTH, HEIGHT);
+    let gui = egui::Window::new("gui")
+        .title_bar(false)
+        .fixed_pos(ZERO)
+        .default_height(HEIGHT)
+        .default_width(WIDTH)
+        .collapsible(false)
+        .resizable(false)
+        .min_height(HEIGHT)
+        .frame(Frame {
+            ..default()
+        });
+    const SIZE: Vec2 = Vec2::new(100.0, 100.0);
+    gui.show(egui_ctx.ctx_mut(), |ui| {
+        ui.vertical_centered(|ui| {
+            let back = ui.button("Back");
+            if back.clicked() {
+                next_state.set(DisplayState::Main);
+            }
+        });
+        ui.horizontal(|ui| {
+                ui.vertical(|ui| {
+                    ui.label("LF");
+                    ui.label("RF");
+                });
+                ui.vertical(|ui| {
+                    ui.label("LR");
+                    ui.label("RR");
+                });
+        });  
+    });
+}
+
+
+fn pedal_menu(
+    mut windows: Query<&mut Window>,
+    mut egui_ctx: EguiContexts,
+    mut next_state: ResMut<NextState<DisplayState>>,
+) {
+    let mut window = windows.single_mut();
+    window.resolution.set(WIDTH, HEIGHT / 2.0);
+}
+
+
+fn main_menu(
+    mut windows: Query<&mut Window>,
+    mut egui_ctx: EguiContexts,
+    mut next_state: ResMut<NextState<DisplayState>>,
+) {
+    let mut window = windows.single_mut();
+    window.resolution.set(WIDTH, HEIGHT);
+    let gui = egui::Window::new("gui")
+        .title_bar(false)
+        .fixed_pos(ZERO)
+        .default_height(HEIGHT)
+        .default_width(WIDTH)
+        .collapsible(false)
+        .resizable(false)
+        .min_height(HEIGHT)
+        .frame(Frame {
+            ..default()
+        });
+    
+    gui.show(egui_ctx.ctx_mut(), |ui| {
+        ui.with_layout(egui::Layout::top_down(egui::Align::Center), |ui| {
+            ui.label("Main Menu");
+            let pedals = ui.button("Pedal Telemetry");
+            let tyres = ui.button("Tyre Telemetry");
+            if pedals.clicked() {
+                next_state.set(DisplayState::Pedals);
+            }
+            if tyres.clicked() {
+                next_state.set(DisplayState::Tyres);
+            }
+        });
+            
+    });
 }
 
 
@@ -102,23 +199,9 @@ fn init(
     window.resolution.set(WIDTH, HEIGHT);
     window.position = WindowPosition::new(IVec2 {x: 0, y: 0});
     window.title = "RBR-GUI".to_string();
+    window.resize_constraints.min_width = WIDTH;
+    window.resize_constraints.min_height = HEIGHT;
 }
-
-fn ui_handler(
-    mut egui_ctx: EguiContexts, 
-    rbr: Res<RBR>
-) {
-    egui::Window::new("")
-        .collapsible(false)
-        .resizable(false)
-        .show(egui_ctx.ctx_mut(), |ui| {
-            ui.horizontal(|ui| {
-                let _ = ui.button("Start!");
-                let _ = ui.button("Exit");
-            })
-        });
-}
-
 
 fn connect_udp(
     mut socket: ResMut<Socket>
