2213f2cc9d3c1e9c771eb6f642ce3c52bbe0f927
MajGucek <maj.gucek@gmail.com>
2024-06-05 17:02:47 +0200

    basic UI

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,20 @@
 use std::net::UdpSocket;
 use std::io::Error;
 use bincode::deserialize;
+// telemetry.rs
 mod telemetry;
 use telemetry::Telemetry;
 
 // UI
-use bevy::{prelude::*, ui::ContentSize};
+use bevy::{prelude::*};
+use egui::{pos2, Color32, Pos2, Rect, Rounding, Vec2};
 use bevy_egui::{EguiContexts, EguiPlugin};
 //use bevy::time::common_conditions::on_timer;
 //use bevy::utils::Duration;
 
 const PORT: &str = "127.0.0.1:6779";
+const WIDTH: f32 = 400.0;
+const HEIGHT: f32 = 100.0;
 
 
 #[derive(Resource)]
@@ -49,6 +53,11 @@ struct Socket {
     socket: Result<UdpSocket, Error>,
     paired: bool,
 }
+impl Socket {
+    fn bind(&mut self) {
+        self.socket = UdpSocket::bind(PORT);
+    }
+}
 
 impl Default for Socket {
     fn default() -> Self {
@@ -60,8 +69,11 @@ impl Default for Socket {
 }
 
 
+
+
 fn main() {
     App::new()
+        .insert_resource(ClearColor(Color::NONE))
         .add_plugins(DefaultPlugins)
         .add_plugins(EguiPlugin)
         .init_resource::<Socket>()
@@ -77,27 +89,41 @@ fn main() {
                     )
         )
         */
+        .add_systems(Startup, init)
         .add_systems(Update, ui_handler)
         .run();
 }
 
 
-fn ui_handler(
-    mut contexts: EguiContexts,
+fn init(
+    mut windows: Query<&mut Window>,
+) {
+    let mut window = windows.single_mut();
+    window.resolution.set(WIDTH, HEIGHT);
+    window.position = WindowPosition::new(IVec2 {x: 0, y: 0});
+    window.title = "RBR-GUI".to_string();
+}
 
+fn ui_handler(
+    mut egui_ctx: EguiContexts, 
     rbr: Res<RBR>
 ) {
-    
-    egui::Window::new("Hello").show(contexts.ctx_mut(), |ui| {
-
-    });
-    
+    egui::Window::new("")
+        .collapsible(false)
+        .resizable(false)
+        .show(egui_ctx.ctx_mut(), |ui| {
+            ui.horizontal(|ui| {
+                let _ = ui.button("Start!");
+                let _ = ui.button("Exit");
+            })
+        });
 }
 
 
 fn connect_udp(
     mut socket: ResMut<Socket>
 ) {
+    socket.bind();
     match socket.socket {
         Ok(_) => {
             socket.paired = true;
